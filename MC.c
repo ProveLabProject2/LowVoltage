4495cc92730515ae4fce26e916f05841cdb91180
Alex Johnson <alexanderchristopherjohnson@gmail.com>
2020-11-03 13:38:30 -0800

    added MC code

---
+++ MC.c
@@ -0,0 +1,62 @@
+
+#include <CAN.h>
+#include <LV.h>
+
+
+void setup() {
+  pinMode(IGNITION_IN, INPUT);
+  pinMode(DIRECTION_IN, INPUT);
+  pinMode(TEMPERATURE, INPUT)
+  
+  pinMode(IGNITION_OUT, OUTPUT);
+  pinMode(DIRECTION_OUT, OUTPUT);
+  pinMode(BRAKE, OUTPUT);
+  pinMode(ACCELERATOR, OUTPUT);
+  
+  Serial.begin(9600);
+  while (!Serial);
+
+  if(debug){
+    Serial.begin(9600);
+    while (!Serial);
+
+    // start the CAN bus at 500 kbps
+    if (!CAN.begin(500E3)) {
+      if(debug)
+        Serial.println("Starting CAN failed!");
+      while (1);
+    }
+
+  // register the receive callback
+  CAN.onReceive(onReceive);
+}
+
+void loop() {
+  bool ignition = analogRead(IGNITION_IN);
+  
+  if(ignition){
+    analogWrite(IGNITION_OUT, ignition);
+	delay(100);
+  }
+}
+
+void onReceive(int packetSize) {
+  Serial.print("Received ");
+  
+  if(ignition){
+  	if (CAN.packetID() == MCU_ID){
+  		unsigned char out = (char)CAN.read();
+  		
+  		if(out == 0x00){
+  		  digitalWrite(BRAKE, 0);
+  		} else if(out == 0x01){
+  		  digitalWrite(BRAKE, 0);
+  		} else{
+  		  analogWrite(ACCELERATOR, out);
+  		}
+  	}
+  }
+  else {
+    Serial.print("Ignition Off");
+  }
+}
