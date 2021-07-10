1d1a3aa4592898a5a9b1ca7e2a4633b921101fa7
Vasanth Sadhasivan <vasanthsadhasivan@gmail.com>
2021-07-09 17:37:18 -0700

    Initial code review

--- mcu.c
+++ mcu.c
@@ -18,19 +18,23 @@ CAN can1 (p30, p29);
 Ticker accelerator_read;
 
 void brake_switch_handler_on(){
+    // Dont know what this is, change to defines. Standardize a format for data being transmitted through CAN. Add defines instead of hardcoded values
     can1.write(CANMessage(0x02,[0x01], 1));
     r_tail = 1;
     l_tail = 1;
 }
 
 void brake_switch_handler_off(){
+    // Dont know what this is, change to defines. Standardize a format for data being transmitted through CAN. Add defines instead of hardcoded values
     can1.write(CANMessage(0x02,[0x00], 1));
     r_tail = 0;
     l_tail = 0;
 }
 
 void accelerator_time_handler(){
+    // Might have to typecase from float to char
     char accelerator_value = accelerator.read()*255;
+    // Add defines and change to standard can msg format
     can1.write(CANMessage(0x02, &accelerator_value, 1));
 }
  
@@ -39,6 +43,7 @@ int main() {
     brake.fall(&brake_switch_handler_off);
 
     accelerator_read.attach(&accelerator_time_handler, 0.1);
+    // Probably have to add a empty while loop here
 }
 
 
